216a8618de901d2ff055e4e28c4c3a1488d5b30f
Jeremy Chone <jeremy.chone@gmail.com>
2017-04-06 11:42:05 -0700

    . update comments DashMainView.js

--- src/view/Dash/DashMainView.js
+++ src/view/Dash/DashMainView.js
@@ -13,8 +13,10 @@ d.register("DashMainView",{
 		var view = this; // best practice, set the view variable first. 
 
 
-		// Note: As a scheduler.add/remoe example, here we add manually a schedule without a namespace, so, 
-		//       we need to store the one created to remove it on .destroy
+		// Add the first schedule with the  scheduler directly with the scheduler.add 
+		// Note 1: Here we add manually a schedule without a namespace, so, 
+		//         we need to store the one created to remove it on .destroy
+		// Important: The recommended way for view is to use the view.schedules below which is enabled by scheduler-hook.js.
 		view.scheduleNs = scheduler.add({
 			performFn: function(){
 				return ajax.get("/api/cpuUsage");
@@ -31,15 +33,14 @@ d.register("DashMainView",{
 	destroy: function(){
 		var view = this;
 		
-		// console.log("DashMainView.destroy", view.scheduleNs);
-
-		// For the manual scheduler, we remove the schedule manually (other ones below will be removed by the scheduler-hook)
+		// For the manual scheduler, we must remove the schedule manually.
 		scheduler.remove(view.scheduleNs);
 	},
 
 
-	// Note: For the other schedules, we use the application scheduler hook that will automatically create and remove 
-	//       the schedules below on the view create and destroy
+
+	// RECOMMENDED: Here we add the other schedule the view.schedules way which is managed by the scheduler-hook.js. 
+	//              Those schedules will be added when the view get created and removed when the view is removed.
 	schedules: [{
 		performFn: function(){
 			return ajax.get("/api/memUsage");
